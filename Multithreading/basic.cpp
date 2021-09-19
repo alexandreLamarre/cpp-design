c66660d00799fe6c76cf2d984ebe1ccdf37ac910
alexandreLamarre <alex7285@gmail.com>
2021-09-19 09:27:41 -0400

    basic multithreading in c++

---
+++ Multithreading/basic.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+/**
+ * @brief New workload for a thread
+ * 
+ */
+void work()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        this_thread::sleep_for(chrono::milliseconds(500));
+        cout << "Loop" << i << endl;
+    }
+}
+
+/**
+ * @brief Unordered (non-synced, non-locked) threading workload
+ * 
+ * @return int exit status
+ */
+int main(){
+    thread t1(work);
+    thread t2(work);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
