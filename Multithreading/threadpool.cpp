33ca17bd6b7493d8e1fc195a5ab8fabfb6fbc664
alexandreLamarre <alex7285@gmail.com>
2021-09-20 08:14:28 -0400

    basic threadpool

--- Multithreading/threadpool.cpp
+++ Multithreading/threadpool.cpp
@@ -4,30 +4,90 @@
 #include <thread>
 #include <vector>
 #include <mutex>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 mutex g_mtx;
+
+template <typename T>
+class blockingQueue
+{
+private:
+    mutex _mtx;
+    condition_variable _cond;
+    int _max_size;
+    queue<T> _q;
+public:
+    blockingQueue(int max_size) : _max_size(max_size){}
+    void push(T t)
+    {
+        unique_lock<mutex> lock(_mtx);
+
+        _cond.wait(lock, [this](){ return _q.size() < _max_size; });
+        _q.push(t);
+
+        lock.unlock();
+        _cond.notify_one();
+    }
+
+    T front()
+    {
+        unique_lock<mutex> lock(_mtx);
+
+        _cond.wait(lock, [this](){ return !_q.empty();});
+
+        return _q.front();
+    }
+
+    void pop()
+    {
+        unique_lock<mutex> lock(_mtx);
+        _cond.wait(lock, [this](){ return !_q.empty(); });
+        
+        _q.pop();
+
+        lock.unlock();
+        _cond.notify_one();
+    }
+
+    int size()
+    {
+        lock_guard<mutex> lock(_mtx);
+        return _q.size();
+    }
+};
 
 int work(int id)
 {
     unique_lock<mutex> lock(g_mtx);
     cout << "Starting " << id << endl;
     lock.unlock();
-    this_thread::sleep_for(chrono::seconds(3));
+
+    int seconds = int((5.0 * rand())/RAND_MAX + 2);
+    this_thread::sleep_for(chrono::seconds(seconds));
     return id;
 }
 
 int main()
 {
-    vector<shared_future<int>> vi;
-    for(int i = 0; i < thread::hardware_concurrency(); i++){
+    blockingQueue<shared_future<int>> vi(2);
+
+    thread t([&](){
+        for(int i = 0; i < 20; i++){
         shared_future <int> f = async(launch::async, work, i);
-        vi.push_back(f);
+        vi.push(f);
+    }
+    });
+
+    for(int i = 0; i < 20; i++)
+    {
+        shared_future<int> f = vi.front();
+        int value = f.get();
+        vi.pop();
+        cout << "Returned : " << value << endl;
     }
 
-    for(auto f : vi)
-    {
-        cout << "Returned : " <<f.get() << endl;
-    }
+    t.join();
     return 0;
 }
