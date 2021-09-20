21e932ca1eab20c9fd782df117b3e85ef9a44ed0
alexandreLamarre <alex7285@gmail.com>
2021-09-20 08:06:26 -0400

    lots of threads

---
+++ Multithreading/threadpool.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <future>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+mutex g_mtx;
+
+int work(int id)
+{
+    unique_lock<mutex> lock(g_mtx);
+    cout << "Starting " << id << endl;
+    lock.unlock();
+    this_thread::sleep_for(chrono::seconds(3));
+    return id;
+}
+
+int main()
+{
+    vector<shared_future<int>> vi;
+    for(int i = 0; i < thread::hardware_concurrency(); i++){
+        shared_future <int> f = async(launch::async, work, i);
+        vi.push_back(f);
+    }
+
+    for(auto f : vi)
+    {
+        cout << "Returned : " <<f.get() << endl;
+    }
+    return 0;
+}
