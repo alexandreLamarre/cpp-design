a4ff191fca0d67d0e9a4b79a72d3264c69a5cfc1
alexandreLamarre <alex7285@gmail.com>
2021-09-24 18:50:42 -0400

    abstract factory example

---
+++ Creational/factory/abstractFactory.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <memory>
+#include <map>
+
+using namespace std;
+
+struct HotDrink
+{
+    virtual ~HotDrink() = default;
+    virtual void prepare(int volume) = 0;
+};
+
+struct Tea : HotDrink
+{
+    void prepare(int volume) override
+    {
+        cout << "Take a tea bag, pour " << volume << endl;
+    }
+};
+
+struct Coffee : HotDrink
+{
+    void prepare(int volume) override
+    {
+        cout << "Grind some beans, boild water, poor " << volume
+             << "ml, maybe add some cream";
+    }
+};
+
+struct HotDrinkFactory
+{
+    virtual unique_ptr<HotDrink> make() const = 0;
+};
+
+struct TeaFactory : HotDrinkFactory
+{
+    unique_ptr<HotDrink> make() const override
+    {
+        return make_unique<Tea>();
+    }
+};
+
+struct CoffeeFactory : HotDrinkFactory
+{
+    unique_ptr<HotDrink> make() const override
+    {
+        return make_unique<Coffee>();
+    }
+};
+
+unique_ptr<HotDrink> make_drink(string type)
+{
+    unique_ptr<HotDrink> drink;
+    if (type == "tea")
+    {
+        drink = make_unique<Tea>();
+        drink->prepare(200);
+    }
+    else
+    {
+        drink = make_unique<Coffee>();
+        drink->prepare(500);
+    }
+    return drink;
+}
+
+class DrinkFactory
+{
+    map<string, unique_ptr<HotDrinkFactory>> hot_factories;
+
+public:
+    DrinkFactory()
+    {
+        hot_factories["coffee"] = make_unique<CoffeeFactory>();
+        hot_factories["tea"] = make_unique<TeaFactory>();
+    }
+
+    unique_ptr<HotDrink> make_drink(const string &name)
+    {
+        auto drink = hot_factories[name]->make();
+        drink->prepare(200);
+        return drink;
+    }
+};
+
+int main(int argc, const char **argv)
+{
+    DrinkFactory df;
+    auto t = df.make_drink("tea");
+    return 0;
+}
