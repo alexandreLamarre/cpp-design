dc4351223f8f029475aab2307b73ce94c76136b4
alexandreLamarre <alex7285@gmail.com>
2021-10-01 15:54:12 -0400

    lexing interpreter

---
+++ Structural/interpreter/lexing.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+struct Token
+{
+    string text;
+    enum Type
+    {
+        integer,
+        plus,
+        minus,
+        lparen,
+        rparen
+    } type;
+
+    explicit Token(Type type, const string &text) : type{type}, text{text} {}
+
+    friend ostream &operator<<(ostream &os, const Token &token)
+    {
+        os << " " << token.text << "";
+        return os;
+    }
+};
+
+vector<Token> lex(const string &input)
+{
+    vector<Token> result;
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        switch (input[i])
+        {
+        case '+':
+            result.push_back(Token{Token::plus, "+"});
+            break;
+        case '-':
+            result.push_back(Token{Token::minus, "-"});
+            break;
+        case '(':
+            result.push_back(Token{Token::lparen, "("});
+            break;
+        case ')':
+            result.push_back(Token{Token::rparen, ")"});
+            break;
+        default:
+            ostringstream buffer;
+            buffer << input[i];
+            for (int j = i + 1; j < input.size(); j++)
+            {
+                if (isdigit(input[j]))
+                {
+                    buffer << input[j];
+                    i++;
+                }
+                else
+                {
+                    result.push_back(Token{Token::integer, buffer.str()});
+                    break;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, const char **argv)
+{
+    string input{"(13-4)-(12+1)"};
+    auto tokens = lex(input);
+
+    for (auto &t : tokens)
+        cout << t << " ";
+    cout << "\n";
+    return 0;
+}
