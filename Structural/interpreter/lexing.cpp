53a1f4561700180a932f22133a6cf35fe56cd741
alexandreLamarre <alex7285@gmail.com>
2021-10-03 09:12:19 -0400

    parsing

--- Structural/interpreter/lexing.cpp
+++ Structural/interpreter/lexing.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <memory>
 
 using namespace std;
 
@@ -64,6 +65,94 @@ vector<Token> lex(const string &input)
         }
     }
 
+    return result;
+}
+
+struct Element
+{
+    virtual int eval() const = 0;
+};
+
+struct Integer : Element
+{
+    int value;
+    Integer(int value) : value(value) {}
+
+    int eval() const override
+    {
+        return value;
+    }
+};
+
+struct BinaryOperation : Element
+{
+    enum Type
+    {
+        addition,
+        subtraction
+    } type;
+    shared_ptr<Element> lhs, rhs;
+
+    int eval() const override
+    {
+        auto left = lhs->eval();
+        auto right = rhs->eval();
+        if (type == addition)
+            return left + right;
+        else
+            return left - right;
+    }
+};
+
+unique_ptr<Element> parse(const vector<Token> &tokens)
+{
+    auto result = make_unique<BinaryOperation>();
+    bool have_lhs = false;
+    for (size_t i = 0; i < tokens.size(); i++)
+    {
+        auto token = tokens[i];
+        switch (token.type)
+        {
+        case Token::integer:
+        {
+            int value = stoi(token.text);
+            auto integer = make_shared<Integer>(value);
+            if (!have_lhs)
+            {
+                result->lhs = integer;
+                have_lhs = true;
+            }
+            else
+                result->rhs = integer;
+        }
+        break;
+        case Token::plus:
+            result->type = BinaryOperation::addition;
+            break;
+        case Token::minus:
+            result->type = BinaryOperation::subtraction;
+            break;
+        case Token::lparen:
+        {
+            int j = i;
+            for (; j < tokens.size(); ++j)
+                if (tokens[j].type == Token::rparen)
+                    break; // found it!
+
+            vector<Token> subexpression(&tokens[i + 1], &tokens[j]);
+            auto element = parse(subexpression);
+            if (!have_lhs)
+            {
+                result->lhs = move(element);
+                have_lhs = true;
+            }
+            else
+                result->rhs = move(element);
+            i = j; // advance
+        }
+        break;
+        }
+    }
     return result;
 }
 
